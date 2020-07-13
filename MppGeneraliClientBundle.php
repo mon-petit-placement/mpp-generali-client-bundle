b8be29532ef139f7a13abce2db60d474d2c88ca5
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2020-07-13 15:30:12 +0200

    Fix bundle class

--- MppGeneraliClientBundle.php
+++ MppGeneraliClientBundle.php
@@ -4,18 +4,8 @@ declare(strict_types=1);
 
 namespace Mpp\GeneraliClientBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MppGeneraliClientBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-    }
-
-    public function getContainerExtension()
-    {
-        return new MppGenraliClientExtension();
-    }
 }
