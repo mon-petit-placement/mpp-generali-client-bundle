0216a56370c65cb116ca4d8fa5975f1b8f36e769
l4 <louis_arnaud.catoire@edu.escpeurope.eu>
2020-09-30 19:20:34 +0200

    remove link to wkhtmltopdf

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,12 +20,6 @@ class Configuration implements ConfigurationInterface
                 ->scalarNode('intermediary_code')->isRequired()->end()
                 ->scalarNode('app_code')->isRequired()->end()
                 ->scalarNode('subscription_code')->isRequired()->end()
-                ->arrayNode('pdf_generator')->isRequired()
-                    ->children()
-                        ->scalarNode('debit_mandate_template_path')->end()
-                        ->scalarNode('debit_mandate_export_path')->end()
-                    ->end()
-                ->end()
             ->end()
         ;
 
