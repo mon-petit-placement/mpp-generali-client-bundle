568ecfd1bcc8d6be884a4365a29bc2dc5d305f8a
Thibault Buathier <thibault.buathier@gmail.com>
2021-06-03 15:43:38 +0200

    add missing getters/setters

--- Model/FondsDesinvestis.php
+++ Model/FondsDesinvestis.php
@@ -8,4 +8,24 @@ class FondsDesinvestis
      * @var FondsDesinvesti
      */
     private $fondsDesinvesti;
+
+    /**
+     * @return FondsDesinvesti
+     */
+    public function getFondsDesinvesti(): FondsDesinvesti
+    {
+        return $this->fondsDesinvesti;
+    }
+
+    /**
+     * @param FondsDesinvesti $fondsDesinvesti
+     *
+     * @return FondsDesinvestis
+     */
+    public function setFondsDesinvesti(FondsDesinvesti $fondsDesinvesti): FondsDesinvestis
+    {
+        $this->fondsDesinvesti = $fondsDesinvesti;
+
+        return $this;
+    }
 }
--- Model/FondsInvestis.php
+++ Model/FondsInvestis.php
@@ -8,4 +8,24 @@ class FondsInvestis
      * @var FondsInvesti
      */
     private $fondsInvesti;
+
+    /**
+     * @return FondsInvesti
+     */
+    public function getFondsInvesti(): FondsInvesti
+    {
+        return $this->fondsInvesti;
+    }
+
+    /**
+     * @param FondsInvesti $fondsInvesti
+     *
+     * @return FondsInvestis
+     */
+    public function setFondsInvesti(FondsInvesti $fondsInvesti): FondsInvestis
+    {
+        $this->fondsInvesti = $fondsInvesti;
+
+        return $this;
+    }
 }
