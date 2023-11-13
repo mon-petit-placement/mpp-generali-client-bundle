b8888c899f1b529605061aa15e82062f3e08d1d6
Dorian CHEREAULT <113115852+80r1an@users.noreply.github.com>
2023-11-13 11:06:35 +0100

    feat: add const for contract status (#42)
    * feat: add const for contract status
    
    * fix: add public

--- Model/Situation.php
+++ Model/Situation.php
@@ -4,6 +4,14 @@ namespace Mpp\GeneraliClientBundle\Model;
 
 class Situation
 {
+    public const LIBELLE_SITUATION_SANS_EFFET = 'Sans Effet';
+
+    public const LIBELLE_SITUATION_EN_COURS = 'En cours';
+
+    public const LIBELLE_SITUATION_RACHETE = 'Racheté';
+
+    public const LIBELLE_SITUATION_RENONCIATION = 'Renonciation';
+
     /**
      * @var string|null
      */
